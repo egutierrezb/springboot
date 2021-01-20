459da773b366be1c7d9089ea231730a2f0179318
Alex Gutierrez Blancarte <egutierrezb@gmai.com>
2021-01-19 19:36:55 -0600

    Comments: entry point for api application and controller

--- src/main/java/com/interview/moonphasesapi/MoonPhasesRepository.java
+++ src/main/java/com/interview/moonphasesapi/MoonPhasesRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+//Use default API methods from Repository
 public interface MoonPhasesRepository extends CrudRepository<MoonPhases, Long> {
 
 }
